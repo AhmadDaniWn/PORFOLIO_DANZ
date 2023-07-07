e149cf6b828d75d579f96dca66f8975859fb25c9
AhmadDaniWn <137415256+AhmadDaniWn@users.noreply.github.com>
2023-07-07 15:22:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// KONTOL MU GEDE
+const navbarNav = document.querySelector('.navbar-nav');
+
+// METU KOYOK PELI MU
+document.querySelector('#buk').onclick = () => {
+  navbarNav.classlist.toggle('active')
+};

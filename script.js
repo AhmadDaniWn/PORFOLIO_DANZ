88e05bd4126fc3fca5980054c3d7adab961d9fa0
Danz x nano <137415256+AhmadDaniWn@users.noreply.github.com>
2024-06-10 06:26:33 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// KONTOL MU GEDE
-const navbarNav = document.querySelector('.navbar-nav');
-
-// METU KOYOK PELI MU
-document.querySelector('#buk').onclick = () => {
-  navbarNav.classlist.toggle('active')
-};
